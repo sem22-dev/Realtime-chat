26e330ab492535401886a5761eb90b0fcfe54aa5
sem22-dev <thotjj22@gmail.com>
2024-08-18 22:22:57 +0530

    updates

--- frontend/src/components/Landingpage.tsx
+++ frontend/src/components/Landingpage.tsx
@@ -9,8 +9,10 @@ const LandingPage: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold mb-8 text-gray-800">Welcome to MessageApp</h1>
-      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+      <h1 className=" text-xl lg:text-3xl font-bold mb-8 px-4 text-center text-gray-800">
+        Connect with friends and colleagues in real-time
+      </h1>
+      <div className="bg-white p-8 rounded-lg shadow-md mx-4 lg:w-96">
         {showLogin ? <LoginForm /> : <SignupForm />}
         <p className="mt-4 text-center">
           {showLogin ? "Don't have an account? " : "Already have an account? "}
@@ -22,7 +24,7 @@ const LandingPage: React.FC = () => {
           </button>
         </p>
       </div>
-      <a href='https://twitter.com/Thotsem22' target='_blank' className='absolute bottom-12 text-green-600'>@sem</a>
+      <a href='https://twitter.com/Thotsem22' target='_blank' className='absolute bottom-12 text-lg text-green-600'>@sem</a>
     </div>
   );
 };
